ba0680a47dc8346c1a542c40d59cf9a6237612da
Horea Christian <chr@chymera.eu>
2023-07-09 15:11:17 -0400

    Writing output to file

--- paper/source/prepare/paperdiff.sh
+++ paper/source/prepare/paperdiff.sh
@@ -14,4 +14,4 @@ fi
 echo "diff-pdf --output-diff=comparison.pdf ${HISTORICAL} ${REEXECUTION1}"
 
 # Command returns error code 1 if the pages are different, which makes sense, except it's the opposite of what we're looking for here.
-diff-pdf -v ${HISTORICAL} ${REEXECUTION1} 2> ../data/paperdiff.log || exit 0
+diff-pdf -v ${HISTORICAL} ${REEXECUTION1} > ../data/paperdiff.log || exit 0
