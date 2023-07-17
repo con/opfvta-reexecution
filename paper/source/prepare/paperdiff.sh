fd00ee357eaf97a342e820348be4c532a45ebce2
Horea Christian <chr@chymera.eu>
2023-07-17 10:59:10 -0400

    Merged make targets for paperdiff log and PDF

--- paper/source/prepare/paperdiff.sh
+++ paper/source/prepare/paperdiff.sh
@@ -13,3 +13,4 @@ fi
 
 # Command returns error code 1 if the pages are different, which makes sense, except it's the opposite of what we're looking for here.
 diff-pdf -v ${HISTORICAL} ${REEXECUTION1} > ../data/paperdiff.log || exit 0
+cp comparison ../data/paperdiff.pdf
