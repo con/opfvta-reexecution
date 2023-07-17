2a489e7793bde7277b0523e821204f6974e5d51b
Horea Christian <chr@chymera.eu>
2023-07-17 13:10:15 -0400

    New figure script and make target for fig diff on page 14

---
+++ paper/source/scripts/diff_fig.sh
@@ -0,0 +1,14 @@
+# Cropbox is:
+#  - left boundary (from left)
+#  - lower boundary (from bottom)
+#  - right boundary (from left)
+#  - top boundary (from bottom)
+
+gs \
+  -o ../figs/diff_fig.pdf \
+  -dFirstPage=14 -dLastPage=14 \
+  -sDEVICE=pdfwrite \
+  -c "[/CropBox [45 621 560 825]" \
+  -c " /PAGES pdfmark" \
+  -f ../data/paperdiff.pdf
+
