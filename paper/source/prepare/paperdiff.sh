d10e8d0aa33ea4ab9824b626f1333b0afadaf48c
Horea Christian <chr@chymera.eu>
2023-07-17 07:03:57 -0400

    Removed trace debugging code

--- paper/source/prepare/paperdiff.sh
+++ paper/source/prepare/paperdiff.sh
@@ -11,7 +11,5 @@ if [[ ! -f  ${REEXECUTION1} ]]; then
 	wget https://articles.chymera.eu/${REEXECUTION1}
 fi
 
-echo "diff-pdf --output-diff=comparison.pdf ${HISTORICAL} ${REEXECUTION1}"
-
 # Command returns error code 1 if the pages are different, which makes sense, except it's the opposite of what we're looking for here.
 diff-pdf -v ${HISTORICAL} ${REEXECUTION1} > ../data/paperdiff.log || exit 0
