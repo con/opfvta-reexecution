b484aa3cede0a7f0dacf34bdd63c849711c3d13f
Horea Christian <chr@chymera.eu>
2023-08-18 14:44:45 -0400

    Use last reexecution for paperdiff analysis

--- paper/source/prepare/paperdiff.sh
+++ paper/source/prepare/paperdiff.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-HISTORICAL="neurohost_opfvta.pdf"
-
-if [[ ! -f  ${HISTORICAL} ]]; then
-	wget https://articles.chymera.eu/${HISTORICAL}
-fi
-
-REEXECUTION1="typhon_opfvta.pdf"
-if [[ ! -f  ${REEXECUTION1} ]]; then
-	wget https://articles.chymera.eu/${REEXECUTION1}
-fi
+LAST_REEXECUTION=$(ls ../../outputs/ | head -n+2 | tail -1)
 
 # Command returns error code 1 if the pages are different, which makes sense, except it's the opposite of what we're looking for here.
-diff-pdf --output-diff="../data/paperdiff.pdf" -v ${HISTORICAL} ${REEXECUTION1} > ../data/paperdiff.log || exit 0
+diff-pdf --output-diff="../data/paperdiff.pdf" -v ../../outputs/original/article.pdf "../../outputs/${LAST_REEXECUTION}/article.pdf" > ../data/paperdiff.log || exit 0
