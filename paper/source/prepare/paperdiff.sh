5ec7da4e64fd2f841d294c3a3f377ec01459ecc7
Horea Christian <chr@chymera.eu>
2023-08-22 13:56:02 -0400

    Do not continue after failure

--- paper/source/prepare/paperdiff.sh
+++ paper/source/prepare/paperdiff.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
 LAST_REEXECUTION=$(ls ../../../outputs/ | head -n -1 | tail -1)
 
 # Command returns error code 1 if the pages are different, which makes sense, except it's the opposite of what we're looking for here.
