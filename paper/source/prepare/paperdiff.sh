5b99a0f01969d094405e57493eccf50d8917fb9c
Horea Christian <chr@chymera.eu>
2023-08-22 11:00:12 -0400

    Corrected input article paths

--- paper/source/prepare/paperdiff.sh
+++ paper/source/prepare/paperdiff.sh
@@ -3,4 +3,4 @@
 LAST_REEXECUTION=$(ls ../../outputs/ | head -n+2 | tail -1)
 
 # Command returns error code 1 if the pages are different, which makes sense, except it's the opposite of what we're looking for here.
-diff-pdf --output-diff="../data/paperdiff.pdf" -v ../../outputs/original/article.pdf "../../outputs/${LAST_REEXECUTION}/article.pdf" > ../data/paperdiff.log || exit 0
+diff-pdf --output-diff="../data/paperdiff.pdf" -v ../../../outputs/original/article.pdf "../../../outputs/${LAST_REEXECUTION}/article.pdf" > ../data/paperdiff.log || exit 0
