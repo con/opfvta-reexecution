7562a638fd40271e22c67c9253308a36d6faa7e6
Horea Christian <chr@chymera.eu>
2023-09-12 11:22:15 -0400

    Tune up paperdiff.sh to centralize paths definition and make it not care about folder from which it is run
    Also made it consistently using ${} around variables
    And correspondingly no longer cd-ing in the Makefile
    
    Also added comments on what to add to manuscript

--- paper/source/prepare/paperdiff.sh
+++ paper/source/prepare/paperdiff.sh
@@ -2,7 +2,12 @@
 
 set -eu -o pipefail
 
-LAST_REEXECUTION=$(ls ../../../outputs/ | head -n -1 | tail -1)
+cd "$(dirname "$0")"
+
+OUTPUT_PATH=../../../outputs
+DATA_PATH=../data
+
+LAST_REEXECUTION=$(ls ${OUTPUT_PATH}/ | head -n -1 | tail -1)
 
 # Command returns error code 1 if the pages are different, which makes sense, except it's the opposite of what we're looking for here.
-diff-pdf --output-diff="../data/paperdiff.pdf" -v ../../../outputs/original/article.pdf "../../../outputs/${LAST_REEXECUTION}/article.pdf" > ../data/paperdiff.log || exit 0
+diff-pdf --output-diff="${DATA_PATH}/paperdiff.pdf" -v ${OUTPUT_PATH}/original/article.pdf "${OUTPUT_PATH}/${LAST_REEXECUTION}/article.pdf" > ${DATA_PATH}/paperdiff.log || exit 0
