7a8d157a99d05acd780962ae75930be4bed628c0
Horea Christian <chr@chymera.eu>
2023-09-19 17:29:24 -0400

    Better handling of exit codes

--- paper/source/prepare/paperdiff.sh
+++ paper/source/prepare/paperdiff.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # We can't have `set -eu` since `diff-pdf` returns a non-zero output code for differing PDFs
-#set -eu -o pipefail
+set -eu -o pipefail
 
 PODMAN_LAST_REEXECUTIONS=($(ls ../../../outputs/podman_*/article.pdf | tail -2))
 SINGULARITY_LAST_REEXECUTIONS=($(ls ../../../outputs/singularity_*/article.pdf | tail -2))
@@ -10,13 +10,13 @@ SINGULARITY_LAST_REEXECUTIONS=($(ls ../../../outputs/singularity_*/article.pdf |
 for i in "${PODMAN_LAST_REEXECUTIONS[@]}"; do
 	echo "GENERATING DIFF FOR EXECUTION in ${i}:"
 	this_execution=$(echo ${i} | grep -Po '.*outputs/\K[^/]*')
-	# Command returns error code 1 if the pages are different, which makes sense, except it's the opposite of what we're looking for here, hence the command below could be run with `|| exit 0` if non-zero exit codes are breaking the loops.
-	diff-pdf --output-diff="../data/paperdiff_${this_execution}.pdf" -v ../../../outputs/original/article.pdf "${i}" > "../data/paperdiff_${this_execution}.log"
+	diff-pdf --output-diff="../data/paperdiff_${this_execution}.pdf" -v ../../../outputs/original/article.pdf "${i}" > "../data/paperdiff_${this_execution}.log" || { echo "Handling non-zero exit code (${?}) for differing documents."; }
 done
 
 for i in "${SINGULARITY_LAST_REEXECUTIONS[@]}"; do
 	echo "GENERATING DIFF FOR EXECUTION in ${i}:"
 	this_execution=$(echo ${i} | grep -Po '.*outputs/\K[^/]*')
-	diff-pdf --output-diff="../data/paperdiff_${this_execution}.pdf" -v ../../../outputs/original/article.pdf "${i}" > "../data/paperdiff_${this_execution}.log"
+	diff-pdf --output-diff="../data/paperdiff_${this_execution}.pdf" -v ../../../outputs/original/article.pdf "${i}" > "../data/paperdiff_${this_execution}.log" || { echo "Handling non-zero exit code (${?}) for differing documents."; }
+
 done
 
