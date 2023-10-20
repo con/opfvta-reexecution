8569f4c10bde85126b979179d5730bc91e494a42
Horea Christian <chr@chymera.eu>
2023-10-20 08:32:58 -0400

    Fully parameterized showcase article

--- article/prepare/paperdiff.sh
+++ article/prepare/paperdiff.sh
@@ -5,6 +5,10 @@ cd "$(dirname "$0")"
 # We can't have `set -eu` since `diff-pdf` returns a non-zero output code for differing PDFs
 set -eu -o pipefail
 
+SHOWCASE_PDF=${1}
+# Pretty fragile... might be better to just pipe the <env>_<data> part, but we don't do that.
+SHOWCASE_PDF=$(echo "${SHOWCASE_PDF}" | sed -e "s:^data/paperdiff_::g")
+
 OUTPUT_PATH="../../outputs"
 DATA_PATH="../data"
 
@@ -13,7 +17,8 @@ SINGULARITY_LAST_REEXECUTIONS=($(ls ${OUTPUT_PATH}/singularity_*/article.pdf | t
 
 # Add execution selected for showcase figures
 # Avoids https://github.com/con/opfvta-replication-2023/issues/41
-SINGULARITY_LAST_REEXECUTIONS+=("${OUTPUT_PATH}/paperdiff_singularity_20230908122618.pdf")
+#SINGULARITY_LAST_REEXECUTIONS+=("${OUTPUT_PATH}/paperdiff_singularity_20230908122618.pdf")
+SINGULARITY_LAST_REEXECUTIONS+=("${OUTPUT_PATH}/${SHOWCASE_PDF}")
 
 # Deduplicate array
 IFS=" " read -r -a SINGULARITY_LAST_REEXECUTIONS <<< "$(tr ' ' '\n' <<< "${SINGULARITY_LAST_REEXECUTIONS[@]}" | sort -u | tr '\n' ' ')"
