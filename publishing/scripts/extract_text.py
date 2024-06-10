4e647ce898e6ad5b8f67c9d6f6cf22ba77912a01
Horea Christian <chr@chymera.eu>
2024-06-10 19:49:25 -0400

    Trying to make script more automated

--- publishing/scripts/extract_text.py
+++ publishing/scripts/extract_text.py
@@ -1,6 +1,11 @@
 from pdfminer.high_level import extract_text
+import re
 
 text = extract_text('article.pdf')
 with open('article.pdf','rb') as f:
-    text = extract_text(f)
-    print(text)
+	text = extract_text(f)
+	print(text)
+	#line_text = text.split("\n")
+	#for i in line_text:
+	#	if re.search('[a-zA-Z]', i):
+	#		print(i)
